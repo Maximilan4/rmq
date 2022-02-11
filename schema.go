69356114cd80513bb6c5da0c5f289616f0e2b795
Maximilan4 <psychose606@gmail.com>
2022-02-11 19:15:41 +0300

    Presets for schema definition

--- schema.go
+++ schema.go
@@ -31,16 +31,51 @@ type (
     Schema struct {
         Queue    *QueueManager
         Exchange *ExchangeManager
+        channel  *amqp.Channel
+    }
+    Preset interface {
+        Apply(*amqp.Channel, *Schema) error
     }
 )
 
+func (sc *Schema) ApplyPresets(presets ...Preset) (err error) {
+    for _, preset := range presets {
+        if err = preset.Apply(sc.channel, sc); err != nil {
+            return
+        }
+    }
+
+    return
+}
+
 func (ek ExchangeKind) String() string {
     return string(ek)
+}
+
+func (dp *DeclareParams) WithDeadLetterExchange(name string) *DeclareParams {
+    if dp.Args == nil {
+        dp.Args = make(amqp.Table)
+    }
+
+    dp.Args["x-dead-letter-exchange"] = name
+
+    return dp
+}
+
+func (dp *DeclareParams) WithDeadLetterRk(key string) *DeclareParams {
+    if dp.Args == nil {
+        dp.Args = make(amqp.Table)
+    }
+
+    dp.Args["x-dead-letter-routing-key"] = key
+
+    return dp
 }
 
 func GetSchema(channel *amqp.Channel) *Schema {
     return &Schema{
         Queue:    NewQueueManager(channel),
         Exchange: NewExchangeManager(channel),
+        channel:  channel,
     }
 }
